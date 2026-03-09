5906c233e26b3b3f163ae8eee1beb1349285ac7e
mjreno <mreno@ucar.edu>
2026-03-09 10:07:22 -0400

    test 10

--- docs/examples/frenchman-flat.py
+++ docs/examples/frenchman-flat.py
@@ -90,6 +90,17 @@ def plot_head_ugrid(head, cbc, grid, workspace):
         grid=ugrid,
     )
 
+    zoomed_uda = head_uda.ugrid.sel(x=slice(590000, 599000), y=slice(4078000, 4085000))
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    zoomed_uda.ugrid.plot(ax=ax, linewidth=0.2, edgecolors="black", cmap="viridis")
+    ax.set_title("Frenchman Flat Head (layer 1, time 0)")
+
+    plt.savefig(workspace / "head_ugrid.png", dpi=300, bbox_inches="tight")
+    if not os.environ.get("PYTEST_CURRENT_TEST"):
+        plt.show()
+    plt.close()
+
     # Flow vectors: u = flow-right-face (+x/east), v = -flow-front-face (+y/north)
     u = cbc["flow-right-face"].isel(time=0, layer=0).compute()
     v = -cbc["flow-front-face"].isel(time=0, layer=0).compute()
@@ -103,22 +114,15 @@ def plot_head_ugrid(head, cbc, grid, workspace):
     ds = ds.ugrid.assign_face_coords()
 
     fig, ax = plt.subplots(figsize=(10, 8))
-    head_uda.ugrid.plot(ax=ax)
-    xu.plot.line(ugrid, ax=ax, color="black", linewidth=0.1)
-    ds.plot.quiver(x="mesh2d_face_x", y="mesh2d_face_y", u="u", v="v", color="black")
-
+    ds.plot.quiver(x="mesh2d_face_x", y="mesh2d_face_y", u="u", v="v", color="black", scale=100)
+    xu.plot.line(ugrid, ax=ax, color="black", linewidth=0.2)
     ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha="right")
-    ax.set_title("Head with flow vectors (layer 1, time 0)")
+    ax.set_title("Frenchman Flat flow vectors (layer 1, time 0)")
 
-    x_min, x_max = 590000, 599000
-    y_min, y_max = 4078000, 4085000
-    ax.set_xlim(x_min, x_max)
-    ax.set_ylim(y_min, y_max)
-    ax.set_aspect(1)
-
-    plt.savefig(workspace / "head_ugrid.png", dpi=300, bbox_inches="tight")
+    plt.savefig(workspace / "flow_ugrid.png", dpi=300, bbox_inches="tight")
     if not os.environ.get("PYTEST_CURRENT_TEST"):
         plt.show()
+
     plt.close()
 
 
